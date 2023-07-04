99a7565042ed42dce9ca1bfe5f2be0b07aad510f
rivaldofez <rivaldofez@gmail.com>
2023-07-04 23:46:59 +0700

    implement launcher activity

---
+++ app/src/main/java/com/icetetik/launcher/LauncherActivity.kt
@@ -0,0 +1,30 @@
+package com.icetetik.launcher
+
+import android.content.Intent
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.os.Handler
+import android.os.Looper
+import com.icetetik.MainActivity
+import com.icetetik.databinding.ActivityLauncherBinding
+
+class LauncherActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityLauncherBinding
+
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        binding = ActivityLauncherBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        Handler(Looper.getMainLooper()).postDelayed({
+            startActivity(Intent(this@LauncherActivity, MainActivity::class.java))
+            finish()
+        }, SPLASH_TIME )
+    }
+
+    companion object {
+        private const val SPLASH_TIME: Long = 3000
+    }
+}
