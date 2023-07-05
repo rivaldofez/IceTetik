f952ccb1d5663d984939bd04a5ca2ebbd3e161ee
rivaldofez <rivaldofez@gmail.com>
2023-07-05 17:58:23 +0700

    add base view of journal with custom calendar

---
+++ app/src/main/java/com/icetetik/journal/JournalActivity.kt
@@ -0,0 +1,12 @@
+package com.icetetik.journal
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.icetetik.R
+
+class JournalActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_journal)
+    }
+}
