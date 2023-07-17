c8f1a31b94bb14b0446fb31b89c6e82091322de9
rivaldofez <rivaldofez@gmail.com>
2023-07-17 21:24:18 +0700

    implement base sign up with email and password

--- app/src/main/java/com/icetetik/authentication/signup/SignUpActivity.kt
+++ app/src/main/java/com/icetetik/authentication/signup/SignUpActivity.kt
@@ -1,12 +1,56 @@
 package com.icetetik.authentication.signup
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Toast
+import com.google.firebase.auth.FirebaseAuth
 import com.icetetik.R
+import com.icetetik.authentication.LoginActivity
+import com.icetetik.databinding.ActivitySignUpBinding
 
 class SignUpActivity : AppCompatActivity() {
+    private lateinit var binding: ActivitySignUpBinding
+    private lateinit var firebaseAuth: FirebaseAuth
+
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_sign_up)
+        binding = ActivitySignUpBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+
+        firebaseAuth = FirebaseAuth.getInstance()
+
+        binding.btnToLogin.setOnClickListener {
+            val intent = Intent(this, LoginActivity::class.java)
+            startActivity(intent)
+        }
+
+
+
+
+        binding.btnSignup.setOnClickListener {
+            val email = binding.edtEmail.text.toString()
+            val pass = binding.edtPassword.text.toString()
+            val confirmPass = binding.edtRetypePassword.toString()
+
+            if (email.isNotEmpty() && pass.isNotEmpty() && confirmPass.isNotEmpty()){
+                if (pass == confirmPass){
+                    firebaseAuth.createUserWithEmailAndPassword(email, pass).addOnCompleteListener {
+                        if (it.isSuccessful){
+                            val intent = Intent(this, LoginActivity::class.java)
+                            startActivity(intent)
+                        } else {
+                            Toast.makeText(this, "failed to create because ${it.exception.toString()}", Toast.LENGTH_SHORT).show()
+                        }
+                    }
+                } else {
+                    Toast.makeText(this, "Pass not match", Toast.LENGTH_SHORT).show()
+                }
+            } else {
+                Toast.makeText(this, "Empty fields not allowed", Toast.LENGTH_SHORT).show()
+            }
+        }
     }
 }
