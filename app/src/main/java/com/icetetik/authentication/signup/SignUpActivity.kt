452ec588306cb85008ba6dac5c7285d5944b1e22
rivaldofez <rivaldofez@gmail.com>
2023-07-17 21:05:52 +0700

    add base layout sign up

---
+++ app/src/main/java/com/icetetik/authentication/signup/SignUpActivity.kt
@@ -0,0 +1,12 @@
+package com.icetetik.authentication.signup
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.icetetik.R
+
+class SignUpActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_sign_up)
+    }
+}
