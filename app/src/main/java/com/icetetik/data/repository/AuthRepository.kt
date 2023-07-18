9fbb4003331817ec1cfc0057ddb4486e479e7f16
rivaldofez <rivaldofez@gmail.com>
2023-07-19 00:18:38 +0700

    add function to sign up on repository

--- app/src/main/java/com/icetetik/data/repository/AuthRepository.kt
+++ app/src/main/java/com/icetetik/data/repository/AuthRepository.kt
@@ -1,4 +1,71 @@
 package com.icetetik.data.repository
 
-class AuthRepository {
+import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.auth.FirebaseAuthInvalidCredentialsException
+import com.google.firebase.auth.FirebaseAuthUserCollisionException
+import com.google.firebase.auth.FirebaseAuthWeakPasswordException
+import com.google.firebase.firestore.FirebaseFirestore
+import com.icetetik.data.model.User
+import com.icetetik.util.FireStoreCollection
+import com.icetetik.util.UiState
+
+class AuthRepository(
+    val auth: FirebaseAuth,
+    val database: FirebaseFirestore
+) {
+
+
+    fun signUpUser(user: User, result: (UiState<String>) -> Unit){
+        auth.createUserWithEmailAndPassword(user.email, user.password)
+            .addOnCompleteListener {
+                if (it.isSuccessful){
+                    saveUserInfo(user) { state ->
+                        when(state) {
+                            is UiState.Success -> {
+                                result.invoke(UiState.Success("User Successfully Registered!"))
+                            }
+
+                            is UiState.Failure -> {
+                                result.invoke(UiState.Failure(state.error))
+                            }
+
+                            is UiState.Loading -> {
+                                result.invoke(UiState.Loading)
+                            }
+                        }
+                    }
+                } else {
+                    try {
+                        throw it.exception ?: java.lang.Exception("Invalid authentication")
+                    } catch (e: FirebaseAuthWeakPasswordException) {
+                        result.invoke(UiState.Failure("Authentication failed, Password should be at least 6 characters"))
+                    } catch (e: FirebaseAuthInvalidCredentialsException) {
+                        result.invoke(UiState.Failure("Authentication failed, Invalid email entered"))
+                    } catch (e: FirebaseAuthUserCollisionException) {
+                        result.invoke(UiState.Failure("Authentication failed, Email already registered."))
+                    } catch (e: Exception) {
+                        result.invoke(UiState.Failure(e.message))
+                    }
+                }
+            }
+
+    }
+
+
+    fun saveUserInfo(user: User, result: (UiState<String>) -> Unit) {
+        val document = database.collection(FireStoreCollection.USER).document(user.email)
+        document.set(user)
+            .addOnSuccessListener {
+                result.invoke(
+                    UiState.Success("User has been save successfully")
+                )
+            }
+            .addOnFailureListener {
+                result.invoke(
+                    UiState.Failure(it.localizedMessage)
+                )
+            }
+    }
+
+
 }
