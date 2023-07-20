fd4898e3bf8690275edaf32468dc98a6de07d582
rivaldofez <rivaldofez@gmail.com>
2023-07-21 03:17:23 +0700

    create activity mood note writer

---
+++ app/src/main/java/com/icetetik/journal/MoodNoteWriterActivity.kt
@@ -0,0 +1,12 @@
+package com.icetetik.journal
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.icetetik.R
+
+class MoodNoteWriterActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_mood_note_writer)
+    }
+}
