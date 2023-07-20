10f8bc197307a468aa110175f06e4ebb49462177
rivaldofez <rivaldofez@gmail.com>
2023-07-21 04:34:23 +0700

    implement toolbar and finishing design note writer

--- app/src/main/java/com/icetetik/journal/MoodNoteWriterActivity.kt
+++ app/src/main/java/com/icetetik/journal/MoodNoteWriterActivity.kt
@@ -3,10 +3,30 @@ package com.icetetik.journal
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import com.icetetik.R
+import com.icetetik.databinding.ActivityMoodNoteWriterBinding
 
 class MoodNoteWriterActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityMoodNoteWriterBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_mood_note_writer)
+
+        binding = ActivityMoodNoteWriterBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        setupToolbar()
+
+    }
+
+    private fun setupToolbar() {
+        setSupportActionBar(binding.toolbar)
+        supportActionBar?.setDisplayShowTitleEnabled(false)
+        supportActionBar?.setDisplayHomeAsUpEnabled(true)
+        supportActionBar?.setDisplayShowHomeEnabled(true)
+
+        binding.toolbar.setNavigationOnClickListener {
+            onBackPressed()
+        }
     }
 }
