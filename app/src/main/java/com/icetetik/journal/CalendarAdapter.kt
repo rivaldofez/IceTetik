dca3117b5a0283c2e2d6857aa871e051728b6c92
rivaldofez <rivaldofez@gmail.com>
2023-07-22 06:42:34 +0700

    change color in calendar

--- app/src/main/java/com/icetetik/journal/CalendarAdapter.kt
+++ app/src/main/java/com/icetetik/journal/CalendarAdapter.kt
@@ -9,7 +9,8 @@ import com.icetetik.R
 import com.icetetik.databinding.ItemCalendarBinding
 import java.time.LocalDate
 
-class CalendarAdapter(val context: Context, val callback: CalendarItemCallback): RecyclerView.Adapter<CalendarAdapter.CalendarViewHolder>() {
+class CalendarAdapter(val context: Context, val callback: CalendarItemCallback) :
+    RecyclerView.Adapter<CalendarAdapter.CalendarViewHolder>() {
     private val daysOfMonth: ArrayList<String> = ArrayList()
     private var currentAdapterDate: LocalDate = LocalDate.now()
     private val currentDate = LocalDate.now()
@@ -17,21 +18,15 @@ class CalendarAdapter(val context: Context, val callback: CalendarItemCallback):
 
 
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): CalendarViewHolder {
-        val binding = ItemCalendarBinding.inflate(LayoutInflater.from(parent.context), parent, false)
+        val binding =
+            ItemCalendarBinding.inflate(LayoutInflater.from(parent.context), parent, false)
         val layoutparams = binding.root.layoutParams
 //        layoutparams.height = (parent.height * 0.07).toInt()
         layoutparams.height = parent.width / 7
         return CalendarViewHolder(binding)
     }
 
-    fun calculateSizeOfView(context: Context): Int {
-
-        val displayMetrics = context.resources.displayMetrics
-        val dpWidth = displayMetrics.widthPixels
-        return (dpWidth / 7) // COLUMN_COUNT would be 4 in your case
-    }
-
-    fun setData(data: List<String>, currentAdapterDate: LocalDate, selectedDate: LocalDate){
+    fun setData(data: List<String>, currentAdapterDate: LocalDate, selectedDate: LocalDate) {
         daysOfMonth.clear()
         daysOfMonth.addAll(data)
         this.currentAdapterDate = currentAdapterDate
@@ -45,34 +40,39 @@ class CalendarAdapter(val context: Context, val callback: CalendarItemCallback):
 
     override fun getItemCount(): Int = daysOfMonth.size
 
-    inner class CalendarViewHolder(private val binding: ItemCalendarBinding): RecyclerView.ViewHolder(binding.root){
-        fun bind(dayOfMonth: String, position: Int){
-            if(position % 7 == 0){
-                binding.tvItemDay.setTypeface(null, Typeface.BOLD)
-            }
+    inner class CalendarViewHolder(private val binding: ItemCalendarBinding) :
+        RecyclerView.ViewHolder(binding.root) {
+        fun bind(dayOfMonth: String, position: Int) {
+            binding.apply {
+                if (position % 7 == 0) {
+                    tvItemDay.setTypeface(null, Typeface.BOLD)
+                }
 
-            if(
-                dayOfMonth == currentDate.dayOfMonth.toString() &&
-                        currentAdapterDate.year == currentDate.year &&
-                        currentAdapterDate.month.value == currentDate.month.value
-            ){
-                binding.clItemDay.setBackgroundColor(context.getColor(R.color.cream_100))
-            }
+                if (
+                    dayOfMonth == currentDate.dayOfMonth.toString() &&
+                    currentAdapterDate.year == currentDate.year &&
+                    currentAdapterDate.month.value == currentDate.month.value
+                ) {
+                    clItemDay.setBackgroundColor(context.getColor(R.color.green_50))
+                    tvItemDay.setTextColor(context.getColor(R.color.white))
+                    tvItemDay.setTypeface(null, Typeface.BOLD)
+                }
 
-            if(
-                dayOfMonth == selectedDate.dayOfMonth.toString() &&
-                currentAdapterDate.year == selectedDate.year &&
-                currentAdapterDate.month.value == selectedDate.month.value
-            ){
-                binding.clItemDay.setBackgroundColor(context.getColor(R.color.black))
-            }
+                if (
+                    dayOfMonth == selectedDate.dayOfMonth.toString() &&
+                    currentAdapterDate.year == selectedDate.year &&
+                    currentAdapterDate.month.value == selectedDate.month.value
+                ) {
+                    clItemDay.setBackgroundColor(context.getColor(R.color.primaryBackgroundColor))
+                    tvItemDay.setTextColor(context.getColor(R.color.white))
+                    tvItemDay.setTypeface(null, Typeface.BOLD)
+                }
 
-
-            binding.tvItemDay.text = dayOfMonth
-
-            if (dayOfMonth.isNotEmpty()){
-                binding.root.setOnClickListener {
-                    callback.onItemCalendarClicked(dayOfMonth)
+                tvItemDay.text = dayOfMonth
+                if (dayOfMonth.isNotEmpty()) {
+                    root.setOnClickListener {
+                        callback.onItemCalendarClicked(dayOfMonth)
+                    }
                 }
             }
         }
