7a30b513f129b1b89324fe70609a05a926cae306
rivaldofez <rivaldofez@gmail.com>
2023-07-21 18:21:11 +0700

    implement view model and fetch monthly statistics

--- app/src/main/java/com/icetetik/statistics/StatisticsActivity.kt
+++ app/src/main/java/com/icetetik/statistics/StatisticsActivity.kt
@@ -3,15 +3,24 @@ package com.icetetik.statistics
 import android.graphics.Color
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
+import android.widget.Toast
+import androidx.activity.viewModels
+import androidx.recyclerview.widget.GridLayoutManager
 import com.github.mikephil.charting.data.BarData
 import com.github.mikephil.charting.data.BarDataSet
 import com.github.mikephil.charting.data.BarEntry
 import com.github.mikephil.charting.utils.ColorTemplate
 import com.icetetik.R
 import com.icetetik.databinding.ActivityStatisticsBinding
+import com.icetetik.util.UiState
+import dagger.hilt.android.AndroidEntryPoint
+import java.time.LocalDate
 
+@AndroidEntryPoint
 class StatisticsActivity : AppCompatActivity() {
     private lateinit var binding: ActivityStatisticsBinding
+    private val viewModel: StatisticsViewModel by viewModels()
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -57,5 +66,37 @@ class StatisticsActivity : AppCompatActivity() {
         barChart.getAxisLeft().setDrawLabels(false) //disable label on right
         barChart.getAxisLeft().axisMinimum = 0F
         barChart.setScaleEnabled(false) //disable zoom
+
+
+        viewModel.getUserSession { email ->
+            if (email == null) {
+
+            } else {
+                viewModel.fetchMonthlyMood(userEmail = email, baseDate = LocalDate.now())
+            }
+        }
+
+        setObserver()
+    }
+
+
+    private fun setObserver(){
+        viewModel.monthlyMood.observe(this) { state ->
+            when(state) {
+                is UiState.Loading -> {
+                    Toast.makeText(this, "Loading", Toast.LENGTH_SHORT).show()
+                }
+
+                is UiState.Failure -> {
+                    Toast.makeText(this, "Error", Toast.LENGTH_SHORT).show()
+                }
+
+                is UiState.Success -> {
+                    Toast.makeText(this, "Success", Toast.LENGTH_SHORT).show()
+                    Log.d("Teston", state.data.toString())
+
+                }
+            }
+        }
     }
 }
--- app/src/main/java/com/icetetik/statistics/StatisticsViewModel.kt
+++ app/src/main/java/com/icetetik/statistics/StatisticsViewModel.kt
@@ -4,19 +4,27 @@ import androidx.lifecycle.LiveData
 import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
 import com.icetetik.data.model.Mood
+import com.icetetik.data.repository.AuthRepository
 import com.icetetik.data.repository.MoodRepository
 import com.icetetik.util.UiState
+import dagger.hilt.android.lifecycle.HiltViewModel
 import java.time.LocalDate
 import javax.inject.Inject
 
+@HiltViewModel
 class StatisticsViewModel @Inject constructor(
-    val moodRepository: MoodRepository
+    val moodRepository: MoodRepository,
+    val authRepository: AuthRepository
 ): ViewModel() {
 
 
     private val _monthlyMood = MutableLiveData<UiState<List<Mood>>>()
     val monthlyMood: LiveData<UiState<List<Mood>>>
         get() = _monthlyMood
+
+    fun getUserSession(result: (String?) -> Unit) {
+        authRepository.getUserSession(result)
+    }
 
     fun fetchMonthlyMood(userEmail: String, baseDate: LocalDate){
         _monthlyMood.value = UiState.Loading
