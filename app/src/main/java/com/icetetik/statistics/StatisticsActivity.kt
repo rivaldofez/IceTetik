7c5027b123516e30f88f07bae2c3e4cd1c4c894a
rivaldofez <rivaldofez@gmail.com>
2023-07-22 05:23:00 +0700

    implement month changer and update bar chart

--- app/src/main/java/com/icetetik/statistics/StatisticsActivity.kt
+++ app/src/main/java/com/icetetik/statistics/StatisticsActivity.kt
@@ -11,7 +11,7 @@ import com.github.mikephil.charting.data.BarData
 import com.github.mikephil.charting.data.BarDataSet
 import com.github.mikephil.charting.data.BarEntry
 import com.icetetik.R
-import com.icetetik.data.model.Mood
+import com.icetetik.data.model.MoodCondition
 import com.icetetik.databinding.ActivityStatisticsBinding
 import com.icetetik.util.Helper
 import com.icetetik.util.UiState
@@ -22,9 +22,8 @@ import java.time.LocalDate
 class StatisticsActivity : AppCompatActivity() {
     private lateinit var binding: ActivityStatisticsBinding
     private val viewModel: StatisticsViewModel by viewModels()
-    private var listMoodDataEntry: ArrayList<BarEntry> = ArrayList()
-
     private var currentLocalDate: LocalDate = LocalDate.now()
+    private var userEmail: String = ""
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -33,16 +32,15 @@ class StatisticsActivity : AppCompatActivity() {
 
         setContentView(binding.root)
         setButtonAction()
-        setMonthView()
+
 
         viewModel.getUserSession { email ->
             if (email == null) {
-
             } else {
-                viewModel.fetchMonthlyMood(userEmail = email, baseDate = LocalDate.now())
+                userEmail = email
+                setMonthView()
             }
         }
-
         setObserver()
     }
 
@@ -60,7 +58,7 @@ class StatisticsActivity : AppCompatActivity() {
 
                 is UiState.Success -> {
                     Toast.makeText(this, "Success", Toast.LENGTH_SHORT).show()
-                    setDataMonthly(state.data)
+                    setupBarChart(state.data)
 
                 }
             }
@@ -83,73 +81,50 @@ class StatisticsActivity : AppCompatActivity() {
 
     private fun setMonthView(){
         binding.tvMonth.text = Helper.monthYearFromDate(currentLocalDate)
+        viewModel.fetchMonthlyMood(userEmail = userEmail, baseDate = currentLocalDate)
     }
 
-    private fun setDataMonthly(monthlyMood: List<Mood>) {
-        var happy = 0
-        var sad = 0
-        var angry = 0
-        var shock = 0
-        var scared = 0
-        var disgusting = 0
+    private fun setupBarChart(monthlyMoodStatistics: Map<MoodCondition, Int>) {
+        if (monthlyMoodStatistics.isEmpty()){
 
-        monthlyMood.forEach { mood ->
-            when (mood.condition) {
-                "Senang" -> happy++
-                "Sedih" -> sad++
-                "Marah" -> angry++
-                "Terkejut" -> shock++
-                "Takut" -> scared++
-                "Jijik" -> disgusting++
-            }
-        }
+        } else {
+            var listMoodDataEntry: ArrayList<BarEntry> = ArrayList()
+            listMoodDataEntry.add(BarEntry(1f, (monthlyMoodStatistics[MoodCondition.HAPPY] ?: 0).toFloat()))
+            listMoodDataEntry.add(BarEntry(2f, (monthlyMoodStatistics[MoodCondition.SAD] ?: 0).toFloat()))
+            listMoodDataEntry.add(BarEntry(3f, (monthlyMoodStatistics[MoodCondition.ANGRY] ?: 0).toFloat()))
+            listMoodDataEntry.add(BarEntry(4f, (monthlyMoodStatistics[MoodCondition.SHOCK] ?: 0).toFloat()))
+            listMoodDataEntry.add(BarEntry(5f, (monthlyMoodStatistics[MoodCondition.SCARED] ?: 0).toFloat()))
+            listMoodDataEntry.add(BarEntry(6f, (monthlyMoodStatistics[MoodCondition.DISGUSTING] ?: 0).toFloat()))
 
-        Log.d("Teston", "Happy" + happy.toString())
-        Log.d("Teston", "Sad" + sad.toString())
-        Log.d("Teston", "Angry" + angry.toString())
-        Log.d("Teston", "Shock" + shock.toString())
-        Log.d("Teston", "Scared" + scared.toString())
-        Log.d("Teston", "Disgusting" + disgusting.toString())
+            val moodBarDataset = BarDataSet(listMoodDataEntry, "Mood")
+            moodBarDataset.setColor(getColor(R.color.cream_75))
+            moodBarDataset.valueTextColor = Color.BLACK
+            moodBarDataset.setDrawValues(false)
 
-        listMoodDataEntry.clear()
-        listMoodDataEntry.add(BarEntry(1f, happy.toFloat()))
-        listMoodDataEntry.add(BarEntry(2f, sad.toFloat()))
-        listMoodDataEntry.add(BarEntry(3f, angry.toFloat()))
-        listMoodDataEntry.add(BarEntry(4f, shock.toFloat()))
-        listMoodDataEntry.add(BarEntry(5f, scared.toFloat()))
-        listMoodDataEntry.add(BarEntry(6f, disgusting.toFloat()))
+            val moodBarChart = binding.bcEmotions
+            moodBarChart.refreshDrawableState()
+            moodBarChart.setFitBars(false)
+            moodBarChart.description.text = "" //set null to description
+            moodBarChart.legend.isEnabled = false //disable legend
 
-        val moodBarDataset = BarDataSet(listMoodDataEntry, "Mood")
-        moodBarDataset.setColor(getColor(R.color.cream_75))
-        moodBarDataset.valueTextColor = Color.BLACK
-        moodBarDataset.setDrawValues(false)
-
-        val moodBarChart = binding.bcEmotions
-        moodBarChart.refreshDrawableState()
-        moodBarChart.setFitBars(false)
-        moodBarChart.description.text = "" //set null to description
-        moodBarChart.legend.isEnabled = false //disable legend
-
-        //        barChart.getAxisRight().setDrawGridLines(false);
-        moodBarChart.getAxisRight().isEnabled = false //disable right axis
-        moodBarChart.xAxis.isEnabled = false
+            //        barChart.getAxisRight().setDrawGridLines(false);
+            moodBarChart.getAxisRight().isEnabled = false //disable right axis
+            moodBarChart.xAxis.isEnabled = false
 //        barChart.getAxisLeft().setDrawGridLines(false);
-        moodBarChart.getXAxis().setDrawGridLines(false)
+            moodBarChart.getXAxis().setDrawGridLines(false)
 
-        moodBarChart.getAxisLeft().setDrawLabels(true) //disable label on right
-
-
-        moodBarChart.getAxisLeft().typeface = ResourcesCompat.getFont(this, R.font.league_spartan_bold)
-        moodBarChart.getAxisLeft().textSize = 16f
-        moodBarChart.getAxisLeft().textColor = Color.WHITE
-
-        moodBarChart.getAxisLeft().axisMinimum = 0F
-        moodBarChart.setScaleEnabled(false) //disable zoom
+            moodBarChart.getAxisLeft().setDrawLabels(true) //disable label on right
+            moodBarChart.getAxisLeft().typeface = ResourcesCompat.getFont(this, R.font.league_spartan_bold)
+            moodBarChart.getAxisLeft().textSize = 16f
+            moodBarChart.getAxisLeft().textColor = Color.WHITE
+            moodBarChart.getAxisLeft().axisMinimum = 0F
+            moodBarChart.setScaleEnabled(false) //disable zoom
 
 
-        val moodBarData = BarData(moodBarDataset)
-        binding.bcEmotions.notifyDataSetChanged()
-        binding.bcEmotions.data = moodBarData
-        binding.bcEmotions.invalidate()
+            val moodBarData = BarData(moodBarDataset)
+            moodBarChart.notifyDataSetChanged()
+            moodBarChart.data = moodBarData
+            moodBarChart.invalidate()
+        }
     }
 }
