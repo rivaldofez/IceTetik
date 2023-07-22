9ded82fe254cb58406e1f49127be8297807fc5f3
rivaldofez <rivaldofez@gmail.com>
2023-07-23 04:26:54 +0700

    add function to load user data in view model

--- app/src/main/java/com/icetetik/settings/SettingsViewModel.kt
+++ app/src/main/java/com/icetetik/settings/SettingsViewModel.kt
@@ -1,7 +1,11 @@
 package com.icetetik.settings
 
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
+import com.icetetik.data.model.User
 import com.icetetik.data.repository.AuthRepository
+import com.icetetik.util.UiState
 import dagger.hilt.android.lifecycle.HiltViewModel
 import javax.inject.Inject
 
@@ -9,4 +13,19 @@ import javax.inject.Inject
 class SettingsViewModel @Inject constructor(
     private val authRepository: AuthRepository
 ): ViewModel() {
+
+    private val _userInfo = MutableLiveData<UiState<User?>>()
+    val userInfo: LiveData<UiState<User?>>
+        get() = _userInfo
+
+
+    fun getUserInfo(userEmail: String){
+        _userInfo.value = UiState.Loading
+
+        authRepository.getUserInfo(userEmail = userEmail){
+            _userInfo.value = it
+        }
+    }
+
+
 }
