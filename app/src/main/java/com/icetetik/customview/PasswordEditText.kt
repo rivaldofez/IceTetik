114efdc2735a3e9cf5499a5c13cbabd315e9992e
rivaldofez <rivaldofez@gmail.com>
2023-07-23 00:29:16 +0700

    add custom password edit text

---
+++ app/src/main/java/com/icetetik/customview/PasswordEditText.kt
@@ -0,0 +1,53 @@
+package com.icetetik.customview
+
+import android.content.Context
+import android.graphics.Canvas
+import android.text.Editable
+import android.text.InputType
+import android.text.TextWatcher
+import android.text.method.PasswordTransformationMethod
+import android.util.AttributeSet
+import androidx.appcompat.widget.AppCompatEditText
+import com.icetetik.R
+
+class PasswordEditText: AppCompatEditText {
+    constructor(context: Context) : super(context){
+        init()
+    }
+
+    constructor(context: Context, attrs: AttributeSet?, defStyleAttr: Int) : super(
+        context,
+        attrs,
+        defStyleAttr
+    ){
+        init()
+    }
+
+    constructor(context: Context, attrs: AttributeSet?) : super(context, attrs){
+        init()
+    }
+
+
+    override fun onDraw(canvas: Canvas?) {
+        super.onDraw(canvas)
+        transformationMethod = PasswordTransformationMethod.getInstance()
+    }
+
+    private fun init(){
+        inputType = InputType.TYPE_TEXT_VARIATION_PASSWORD
+        hint = "Password"
+
+
+        addTextChangedListener(object : TextWatcher {
+            override fun beforeTextChanged(p0: CharSequence?, p1: Int, p2: Int, p3: Int) {}
+
+            override fun onTextChanged(p0: CharSequence?, p1: Int, p2: Int, p3: Int) {
+                if (!p0.isNullOrEmpty() && p0.length < 8 ){
+                    error = "Password minimum 8 characters"
+                }
+            }
+
+            override fun afterTextChanged(p0: Editable?) {}
+        })
+    }
+}
