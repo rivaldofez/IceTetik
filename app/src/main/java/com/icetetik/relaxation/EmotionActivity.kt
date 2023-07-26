d2a7dd5a0056d125298c812542ab8c141c9c4e2e
rivaldofez <rivaldofez@gmail.com>
2023-07-27 05:05:50 +0700

    add base layout emotion activity

---
+++ app/src/main/java/com/icetetik/relaxation/EmotionActivity.kt
@@ -0,0 +1,12 @@
+package com.icetetik.relaxation
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.icetetik.R
+
+class EmotionActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_emotion)
+    }
+}
