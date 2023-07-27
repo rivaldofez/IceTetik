9b4589dc26f40662092271dc236ea531e3d907dd
rivaldofez <rivaldofez@gmail.com>
2023-07-28 06:02:38 +0700

    implement layout onboard relaxation

---
+++ app/src/main/java/com/icetetik/relaxation/OnboardRelaxationFragment.kt
@@ -0,0 +1,18 @@
+package com.icetetik.relaxation
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import com.icetetik.R
+
+class OnboardRelaxationFragment : Fragment() {
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_onboard_relaxation, container, false)
+    }
+}
