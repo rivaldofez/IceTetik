ad87a3085bdb18d66dee25acf15910ebf003f16b
rivaldofez <rivaldofez@gmail.com>
2023-07-28 06:17:41 +0700

    add navigation from onboard to emotion fragment

--- app/src/main/java/com/icetetik/relaxation/OnboardRelaxationFragment.kt
+++ app/src/main/java/com/icetetik/relaxation/OnboardRelaxationFragment.kt
@@ -5,14 +5,36 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.navigation.fragment.findNavController
 import com.icetetik.R
+import com.icetetik.databinding.FragmentOnboardRelaxationBinding
 
 class OnboardRelaxationFragment : Fragment() {
+    private var _binding: FragmentOnboardRelaxationBinding? = null
+    private val binding get() = _binding!!
+
+
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_onboard_relaxation, container, false)
+        _binding = FragmentOnboardRelaxationBinding.inflate(inflater, container, false)
+        return binding.root
     }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+
+        binding.btnNext.setOnClickListener {
+            val goToEmotionFragment = OnboardRelaxationFragmentDirections.actionOnboardRelaxationFragmentToEmotionFragment()
+            findNavController().navigate(goToEmotionFragment)
+        }
+    }
+
+    override fun onDestroyView() {
+        super.onDestroyView()
+        _binding = null
+    }
+
 }
