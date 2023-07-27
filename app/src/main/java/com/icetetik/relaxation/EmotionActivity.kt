a175e3ec2ccef725fc3c743aa2c9dae8e3b59ba1
rivaldofez <rivaldofez@gmail.com>
2023-07-27 17:45:34 +0700

    add flow selected chip item

--- app/src/main/java/com/icetetik/relaxation/EmotionActivity.kt
+++ app/src/main/java/com/icetetik/relaxation/EmotionActivity.kt
@@ -1,9 +1,17 @@
 package com.icetetik.relaxation
 
+import android.content.res.ColorStateList
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
+import android.view.View
 import android.widget.Button
+import androidx.core.content.ContextCompat
+import androidx.core.view.ViewCompat
+import androidx.core.view.children
+import androidx.core.view.get
 import com.google.android.material.chip.Chip
+import com.google.android.material.chip.ChipDrawable
 import com.icetetik.R
 import com.icetetik.databinding.ActivityEmotionBinding
 
@@ -22,7 +30,15 @@ class EmotionActivity : AppCompatActivity() {
 
         titleButton.forEach {
             val chip = Chip(this@EmotionActivity)
+            chip.id = ViewCompat.generateViewId()
             chip.text = it
+            chip.setEnsureMinTouchTargetSize(false)
+            chip.isCheckable = true
+//            chip.setOnCheckedChangeListener { buttonView, isChecked ->
+//                Log.d("Teston", "checked on ${chip.text.toString()}")
+//
+//            }
+
             positiveEmotion.add(chip)
         }
 
@@ -30,5 +46,24 @@ class EmotionActivity : AppCompatActivity() {
             binding.cgPositiveEmotion.addView(it)
         }
 
+
+        for(i in 0 until binding.cgPositiveEmotion.childCount){
+            val chip = binding.cgPositiveEmotion.getChildAt(i) as Chip
+            chip.setOnClickListener {
+                chip.isCheckable = !chip.isCheckable
+                if(chip.isCheckable){
+                    chip.chipBackgroundColor = ColorStateList.valueOf(ContextCompat.getColor(this@EmotionActivity, R.color.white))
+                    Log.d("Teston", "called in white")
+                } else {
+                    chip.chipBackgroundColor = ColorStateList.valueOf(ContextCompat.getColor(this@EmotionActivity, R.color.cream_100))
+                    Log.d("Teston", "called in cream")
+                }
+                Log.d("Teston", "called in loop cg emotion")
+            }
+        }
+
+        binding.cgPositiveEmotion.setOnCheckedChangeListener { group, checkedId ->
+            Log.d("Teston", "called")
+        }
     }
 }
