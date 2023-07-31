73ef81147a251c3ef50fb8e0d5bc8bed949382a3
rivaldofez <rivaldofez@gmail.com>
2023-07-31 08:20:11 +0700

    add intent to onboard video info activity

---
+++ app/src/main/java/com/icetetik/page/questionnaire/OnboardVideoInfoActivity.kt
@@ -0,0 +1,12 @@
+package com.icetetik.page.questionnaire
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.icetetik.R
+
+class OnboardVideoInfoActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_onboard_video_info)
+    }
+}
