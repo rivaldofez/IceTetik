d673f859ce6d3184a147b77a83a5abbb62544740
rivaldofez <rivaldofez@gmail.com>
2023-08-01 16:44:02 +0700

    implement change state theme on settings page

--- app/src/main/java/com/icetetik/page/settings/SettingsActivity.kt
+++ app/src/main/java/com/icetetik/page/settings/SettingsActivity.kt
@@ -7,21 +7,31 @@ import android.graphics.drawable.ColorDrawable
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.provider.Settings
+import android.view.View
 import android.view.Window
+import android.view.WindowManager
+import android.widget.AdapterView
+import android.widget.ArrayAdapter
 import androidx.activity.viewModels
+import androidx.appcompat.app.AppCompatDelegate
 import androidx.core.content.ContentProviderCompat.requireContext
 import androidx.databinding.DataBindingUtil
+import androidx.lifecycle.lifecycleScope
 import com.icetetik.R
 import com.icetetik.page.authentication.AuthenticationActivity
 import com.icetetik.data.model.User
 import com.icetetik.databinding.ActivitySettingsBinding
+import com.icetetik.databinding.SublayoutAlertDialogBinding
 import com.icetetik.databinding.SublayoutDialogConfirmationBinding
+import com.icetetik.databinding.SublayoutDialogThemeBinding
 import com.icetetik.util.Extension.animateChangeVisibility
 import com.icetetik.util.Extension.showShortToast
 import com.icetetik.util.Extension.showSnackBar
 import com.icetetik.util.UiState
 import dagger.hilt.android.AndroidEntryPoint
 import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.flow.collect
+import kotlinx.coroutines.launch
 
 @AndroidEntryPoint
 class SettingsActivity : AppCompatActivity() {
@@ -114,7 +124,7 @@ class SettingsActivity : AppCompatActivity() {
             }
 
             rowTheme.root.setOnClickListener {
-
+                showThemeDialog("Pilih tema favorit kamu")
             }
 
             rowFeedback.root.setOnClickListener {
@@ -164,6 +174,69 @@ class SettingsActivity : AppCompatActivity() {
 
             btnNo.setOnClickListener {
                 dialog.dismiss()
+            }
+        }
+        dialog.show()
+    }
+
+    private fun showThemeDialog(message: String) {
+        val dialogBinding = SublayoutDialogThemeBinding.inflate(layoutInflater)
+
+        val dialog = Dialog(this@SettingsActivity)
+        dialog.requestWindowFeature(Window.FEATURE_NO_TITLE)
+        dialog.setCancelable(false)
+        dialog.setContentView(dialogBinding.root)
+        dialog.window?.setBackgroundDrawable(ColorDrawable(Color.TRANSPARENT))
+//        dialog.window?.setLayout(WindowManager.LayoutParams.MATCH_PARENT, WindowManager.LayoutParams.WRAP_CONTENT)
+
+        dialogBinding.apply {
+            tvDialogMessage.text = message
+
+            val themeTitles = resources.getStringArray(R.array.themes)
+            val arrayAdapter = ArrayAdapter(
+                this@SettingsActivity, R.layout.item_spinner_theme,
+                themeTitles
+            )
+
+            dialogBinding.spnTheme.adapter = arrayAdapter
+            dialogBinding.spnTheme.isSelected = false
+            var selectedTheme: Int? = null
+
+                lifecycleScope.launch {
+                viewModel.getThemeSetting().collect { theme ->
+                    if (!theme.isNullOrEmpty()){
+                        selectedTheme = theme.toInt()
+                        selectedTheme?.let {
+                            dialogBinding.spnTheme.setSelection(it)
+                        }
+                    }
+                }
+            }
+
+            dialogBinding.spnTheme.onItemSelectedListener = object : AdapterView.OnItemSelectedListener {
+                override fun onItemSelected(
+                    parent: AdapterView<*>?,
+                    view: View?,
+                    position: Int,
+                    id: Long
+                ) {
+                    selectedTheme = position
+                }
+
+                override fun onNothingSelected(parent: AdapterView<*>?) {}
+            }
+
+
+            btnClose.setOnClickListener {
+                selectedTheme?.let {
+                    when(it){
+                        0 -> AppCompatDelegate.setDefaultNightMode(AppCompatDelegate.MODE_NIGHT_FOLLOW_SYSTEM)
+                        1 -> AppCompatDelegate.setDefaultNightMode(AppCompatDelegate.MODE_NIGHT_NO)
+                        else ->  AppCompatDelegate.setDefaultNightMode(AppCompatDelegate.MODE_NIGHT_YES)
+                    }
+                    viewModel.saveThemeSetting(themeId = it)
+                    dialog.dismiss()
+                }
             }
         }
         dialog.show()
