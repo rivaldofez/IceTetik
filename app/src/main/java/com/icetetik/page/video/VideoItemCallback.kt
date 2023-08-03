93ab562f3ea1c3744cad8ca9f353de36ef4d2327
rivaldofez <rivaldofez@gmail.com>
2023-08-03 16:28:32 +0700

    fetch data video from database and show on list

--- app/src/main/java/com/icetetik/page/video/VideoItemCallback.kt
+++ app/src/main/java/com/icetetik/page/video/VideoItemCallback.kt
@@ -1,8 +1,7 @@
 package com.icetetik.page.video
 
-import com.icetetik.data.model.MoodItemView
 import com.icetetik.data.model.Video
 
 interface VideoItemCallback {
-    fun onItemVideoCallback(video: Video)
+    fun onItemVideoClick(video: Video)
 }
--- app/src/main/java/com/icetetik/page/video/VideoViewModel.kt
+++ app/src/main/java/com/icetetik/page/video/VideoViewModel.kt
@@ -2,6 +2,7 @@ package com.icetetik.page.video
 
 import androidx.lifecycle.LiveData
 import androidx.lifecycle.MutableLiveData
+import androidx.lifecycle.ViewModel
 import com.icetetik.data.model.Video
 import com.icetetik.data.repository.VideoRepository
 import com.icetetik.util.UiState
@@ -11,7 +12,7 @@ import javax.inject.Inject
 @HiltViewModel
 class VideoViewModel @Inject constructor(
     private val videoRepository: VideoRepository
-) {
+): ViewModel() {
 
     private val _videos = MutableLiveData<UiState<List<Video>>>()
     val videos: LiveData<UiState<List<Video>>>
