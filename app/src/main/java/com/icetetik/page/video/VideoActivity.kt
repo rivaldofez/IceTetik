bbfaa44c13f9173219769cc2f9726504e95883d1
rivaldofez <rivaldofez@gmail.com>
2023-08-03 15:49:21 +0700

    add video view model

--- app/src/main/java/com/icetetik/page/video/VideoActivity.kt
+++ app/src/main/java/com/icetetik/page/video/VideoActivity.kt
@@ -3,10 +3,15 @@ package com.icetetik.page.video
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import com.icetetik.R
+import com.icetetik.databinding.ActivityVideoBinding
 
 class VideoActivity : AppCompatActivity() {
+    private lateinit var binding: ActivityVideoBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_video)
+        binding = ActivityVideoBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
     }
 }
---
+++ app/src/main/java/com/icetetik/page/video/VideoViewModel.kt
@@ -0,0 +1,36 @@
+package com.icetetik.page.video
+
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.MutableLiveData
+import com.icetetik.data.model.Video
+import com.icetetik.data.repository.VideoRepository
+import com.icetetik.util.UiState
+import dagger.hilt.android.lifecycle.HiltViewModel
+import javax.inject.Inject
+
+@HiltViewModel
+class VideoViewModel @Inject constructor(
+    private val videoRepository: VideoRepository
+) {
+
+    private val _videos = MutableLiveData<UiState<List<Video>>>()
+    val videos: LiveData<UiState<List<Video>>>
+        get() = _videos
+
+    private val _addVideos = MutableLiveData<UiState<String>>()
+    val addVideos: LiveData<UiState<String>>
+        get() = _addVideos
+
+
+    fun getVideos(){
+        _videos.value = UiState.Loading
+        videoRepository.getVideos { _videos.value = it }
+    }
+
+    fun addVideos(){
+        _addVideos.value = UiState.Loading
+        videoRepository.setVideos { _addVideos.value = it }
+    }
+
+
+}
