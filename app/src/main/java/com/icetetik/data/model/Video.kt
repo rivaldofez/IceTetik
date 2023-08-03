13bc17f59dc911db8e8d0d865b8d862c8f139af5
rivaldofez <rivaldofez@gmail.com>
2023-08-03 14:02:23 +0700

    add video model and video activity

---
+++ app/src/main/java/com/icetetik/data/model/Video.kt
@@ -0,0 +1,9 @@
+package com.icetetik.data.model
+
+import com.google.firebase.Timestamp
+
+data class Video(
+    val id: String = "",
+    val title: String = "",
+    val url: String = ""
+)
---
+++ app/src/main/java/com/icetetik/page/video/VideoActivity.kt
@@ -0,0 +1,12 @@
+package com.icetetik.page.video
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.icetetik.R
+
+class VideoActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_video)
+    }
+}
