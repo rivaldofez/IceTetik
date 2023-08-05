e940552c2a266d2f89fd12a149567f686dc32ae3
rivaldofez <rivaldofez@gmail.com>
2023-08-05 08:44:28 +0700

    add external link to social media and other platform

--- app/src/main/java/com/icetetik/page/settings/FeedbackActivity.kt
+++ app/src/main/java/com/icetetik/page/settings/FeedbackActivity.kt
@@ -1,12 +1,39 @@
 package com.icetetik.page.settings
 
+import android.content.Intent
+import android.net.Uri
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import com.icetetik.R
+import com.icetetik.databinding.ActivityFeedbackBinding
 
 class FeedbackActivity : AppCompatActivity() {
+    private lateinit var binding: ActivityFeedbackBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_feedback)
+
+        binding = ActivityFeedbackBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        setButtonActions()
+    }
+
+    private fun setButtonActions() {
+        binding.apply {
+            btnGform.setOnClickListener {
+                val url = "https://docs.google.com/forms/d/e/1FAIpQLSf2l1feA6XKBsUjJENbUknRZpOj01Lz4v8AW8Zl4KNoC9IMOQ/viewform"
+                val intent = Intent(Intent.ACTION_VIEW)
+                intent.data = Uri.parse(url)
+                startActivity(intent)
+            }
+
+            btnLinktree.setOnClickListener {
+                val url = "https://linktr.ee/icetetik.co"
+                val intent = Intent(Intent.ACTION_VIEW)
+                intent.data = Uri.parse(url)
+                startActivity(intent)
+            }
+        }
     }
 }
