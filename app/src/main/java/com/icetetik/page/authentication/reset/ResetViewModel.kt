4d985abf1a08d9b8f32bd7496819f06beaa8bc50
rivaldofez <rivaldofez@gmail.com>
2023-08-06 00:37:54 +0700

    add reset password method in view model

---
+++ app/src/main/java/com/icetetik/page/authentication/reset/ResetViewModel.kt
@@ -0,0 +1,29 @@
+package com.icetetik.page.authentication.reset
+
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.MutableLiveData
+import androidx.lifecycle.ViewModel
+import com.icetetik.data.repository.AuthRepository
+import com.icetetik.util.UiState
+import dagger.hilt.android.lifecycle.HiltViewModel
+import javax.inject.Inject
+
+@HiltViewModel
+class ResetViewModel @Inject constructor(
+    private val authRepository: AuthRepository
+): ViewModel() {
+
+    private val _resetPasswordUser = MutableLiveData<UiState<String>>()
+    val resetPasswordUser: LiveData<UiState<String>>
+        get() = _resetPasswordUser
+
+
+    fun resetPasswordUser(email: String){
+        _resetPasswordUser.value = UiState.Loading
+        authRepository.resetPasswordUser(email) {
+            _resetPasswordUser.value = it
+        }
+    }
+
+
+}
