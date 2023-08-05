09fd8df7f07ecc80ce4275e79bc0439b564f2cf7
rivaldofez <rivaldofez@gmail.com>
2023-08-06 00:25:24 +0700

    add reset password method on repository

--- app/src/main/java/com/icetetik/data/repository/AuthRepository.kt
+++ app/src/main/java/com/icetetik/data/repository/AuthRepository.kt
@@ -6,6 +6,7 @@ import com.google.firebase.auth.FirebaseAuthUserCollisionException
 import com.google.firebase.auth.FirebaseAuthWeakPasswordException
 import com.google.firebase.firestore.FirebaseFirestore
 import com.icetetik.data.model.User
+import com.icetetik.util.Extension.showShortToast
 import com.icetetik.util.FireStoreCollection
 import com.icetetik.util.UiState
 
@@ -13,6 +14,26 @@ class AuthRepository(
     private val auth: FirebaseAuth,
     private val database: FirebaseFirestore
 ) {
+
+
+    fun resetPasswordUser(email: String, result: (UiState<String>) -> Unit) {
+        auth.sendPasswordResetEmail(email)
+            .addOnCompleteListener {
+                if (it.isSuccessful) {
+                    result.invoke(UiState.Success("Successfully reset password, please check your email to setup new password"))
+                } else {
+                    result.invoke(UiState.Failure("Reset password failed, please try again later"))
+                }
+            }
+            .addOnFailureListener {
+                result.invoke(
+                    UiState.Failure(
+                        it.localizedMessage
+                    )
+                )
+            }
+    }
+
 
     fun signUpUser(user: User, result: (UiState<String>) -> Unit) {
         auth.createUserWithEmailAndPassword(user.email, user.password)
@@ -37,7 +58,7 @@ class AuthRepository(
                     try {
                         throw it.exception ?: java.lang.Exception("Invalid authentication")
                     } catch (e: FirebaseAuthWeakPasswordException) {
-                        result.invoke(UiState.Failure("Authentication failed, Password should be at least 6 characters"))
+                        result.invoke(UiState.Failure("Authentication failed, Password should be at least 8 characters"))
                     } catch (e: FirebaseAuthInvalidCredentialsException) {
                         result.invoke(UiState.Failure("Authentication failed, Invalid email entered"))
                     } catch (e: FirebaseAuthUserCollisionException) {
@@ -50,7 +71,7 @@ class AuthRepository(
     }
 
 
-    fun signInUser(email: String, password: String, result: (UiState<String>) -> Unit){
+    fun signInUser(email: String, password: String, result: (UiState<String>) -> Unit) {
         auth.signInWithEmailAndPassword(email, password)
             .addOnCompleteListener { task ->
                 if (task.isSuccessful) {
@@ -64,10 +85,10 @@ class AuthRepository(
             }
     }
 
-    fun signOutUser(result: (UiState<String>) -> Unit){
+    fun signOutUser(result: (UiState<String>) -> Unit) {
         auth.signOut()
         val currentUser = auth.currentUser
-        if (currentUser == null){
+        if (currentUser == null) {
             result.invoke(UiState.Success("Successfully Sign Out"))
         } else {
             result.invoke(
@@ -79,9 +100,9 @@ class AuthRepository(
     }
 
 
-    fun getUserSession(result: (String?) -> Unit){
+    fun getUserSession(result: (String?) -> Unit) {
         val currentUser = auth.currentUser
-        if (currentUser == null){
+        if (currentUser == null) {
             result.invoke(null)
         } else {
             result.invoke(
@@ -90,12 +111,12 @@ class AuthRepository(
         }
     }
 
-    fun getUserInfo(userEmail: String, result: (UiState<User?>) -> Unit){
+    fun getUserInfo(userEmail: String, result: (UiState<User?>) -> Unit) {
         database.collection((FireStoreCollection.USER)).document(userEmail)
             .get()
             .addOnSuccessListener { snapshot ->
                 val dataResult = snapshot.toObject(User::class.java)
-                if (dataResult == null){
+                if (dataResult == null) {
                     result.invoke(UiState.Success(null))
                 } else {
                     result.invoke(UiState.Success(dataResult))
