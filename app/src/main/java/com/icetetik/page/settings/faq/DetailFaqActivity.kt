5925bc48675031421ad78ede68772eb50e2457f7
rivaldofez <rivaldofez@gmail.com>
2023-08-08 03:38:55 +0700

    add detail faq activity

--- app/src/main/java/com/icetetik/page/settings/faq/DetailFaqActivity.kt
+++ app/src/main/java/com/icetetik/page/settings/faq/DetailFaqActivity.kt
@@ -1,12 +1,12 @@
-package com.icetetik.page.settings
+package com.icetetik.page.settings.faq
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import com.icetetik.R
 
-class FaqActivity : AppCompatActivity() {
+class DetailFaqActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_faq)
+        setContentView(R.layout.activity_detail_faq)
     }
 }
---
+++ app/src/main/java/com/icetetik/page/settings/faq/FaqActivity.kt
@@ -0,0 +1,12 @@
+package com.icetetik.page.settings.faq
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.icetetik.R
+
+class FaqActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_faq)
+    }
+}
